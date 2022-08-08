b93e5ca55e6718c5f549550f7928bc2cd87f0895
DTEQ46 <olasehindedaniel109@gmail.com>
2022-08-08 05:09:16 -0400

    adjust mobile

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "98b7113d1fda2c89"; }
+/******/ 		__webpack_require__.h = function() { return "4412c2f86d23cf89"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
